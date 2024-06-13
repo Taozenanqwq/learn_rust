26a98b6d5964922d2f5eb9c7ae012cdae662b981
taozenan <taozenan@baidu.com>
2024-06-13 14:50:18 +0800

    feat: rust学习项目初始化

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    //Collection types: Array, Vec, Tuples
+    //Array
+    let my_array = ["one", "two"];
+    //Vec
+    let vec = vec![1, 2, 3];
+    //Tuples
+    let randam_tuple = (7, 8, "this is a str", [7, 8, 9], vec![1, 2, 3]);
+}
+
+
